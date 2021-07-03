ddfe0565e6b39f50e590a5bfc90ac02e0e8d5153
ydn2856 <ydn2856@gmail.com>
2021-07-03 21:34:02 +0800

    Update geoip2.sh

--- geoip2.sh
+++ geoip2.sh
@@ -1,3 +1,11 @@
 #/bin/sh
 
 apt-get install -y git
+git clone https://github.com/ar414-com/nginx-geoip2 /tmp/geoip
+cd /tmp/geoip
+tar -zxvf GeoLite2-City_20200519.tar.gz
+tar -zxvf GeoLite2-Country_20200519.tar.gz
+mkdir /var/geoip2
+mv ./GeoLite2-Country_20200519/GeoLite2-Country.mmdb /var/geoip2
+mv ./GeoLite2-City_20200519/GeoLite2-City.mmdb /var/geoip2
+
